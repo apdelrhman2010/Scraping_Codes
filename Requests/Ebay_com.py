04fdc4eb4357b9853b4c25dd35a753e6b5c6247b
apdelrhman2010 <dodoosos603@gmail.com>
2022-08-11 15:12:43 -0400

    ll

--- Requests/Ebay_com.py
+++ Requests/Ebay_com.py
@@ -1,8 +1,18 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import pandas as bd
 
 def extract(page):
     url = f'https://www.ebay.com/b/Desktops-All-In-One-Computers/171957/bn_1643067?_pgn={page}'
     headers = {
         'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0'
     }
+    r = requests.get(url, headers)
+    soup = bs(r.content, 'html.parser')
+    return soup
+def parse(soup):
+    divs = soup.find_all('div', class_='s-item s-item--large')
+    return len(divs)
+
+c = extract(1)
+print(parse(c))
