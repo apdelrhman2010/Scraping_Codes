ad17ac2f5f3347c15a31b4035b34e08ba1d0b889
ahmedmahmed8a <ahmed@ahmedc00.tech>
2022-08-12 00:20:38 +0200

    fixing

--- Requests/Ebay_com.py
+++ Requests/Ebay_com.py
@@ -7,12 +7,13 @@ def extract(page):
     headers = {
         'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0'
     }
-    r = requests.get(url, headers)
+    r = requests.get(url, headers=headers)
+    print(r)
     soup = bs(r.content, 'html.parser')
     return soup
 def parse(soup):
     divs = soup.find_all('div', class_='s-item s-item--large')
     return len(divs)
-
+print('hi')
 c = extract(1)
-print(parse(c))
+print(c)
